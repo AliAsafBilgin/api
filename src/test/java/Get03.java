1448f13ecc86331aebddc6ac7c167fe5bfd24962
Ali Asaf Bilgin <ali19.bilgin31@gmail.com>
2022-10-27 20:50:37 +0300

    api1

---
+++ src/test/java/Get03.java
@@ -0,0 +1,50 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class Get03 extends JsonplaceholderBaseUrl{
+
+    /*
+      Given
+          https://jsonplaceholder.typicode.com/todos/23
+      When
+          User send GET Request to the URL
+      Then
+          HTTP Status Code should be 200
+And
+    Response format should be "application/json"
+And
+    "title" is "et itaque necessitatibus maxime molestiae qui quas velit",
+And
+    "completed" is false
+And
+    "userId" is 2
+   */
+
+    @Test
+    public void Get01() {
+        //  i)  Set the URL,
+        spec.pathParams("first", "todos", "second",23);
+
+
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("title", equalTo("et itaque necessitatibus maxime molestiae qui quas velit"))
+                .body("completed", equalTo(false))
+                .body("userId", equalTo(2));
+
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON)
+                .body("title", equalTo("et itaque necessitatibus maxime molestiae qui quas velit")
+                        ,"completed", equalTo(false), "userId", equalTo(2));
+
+
+    }
+}
